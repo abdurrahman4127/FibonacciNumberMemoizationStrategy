a377460688bc0bf983dac6adeaeb0075b822d1ed
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-02-19 17:29:16 +0600

    .

--- src/GeneralWay.java
+++ src/GeneralWay.java
@@ -1,5 +1,6 @@
 /*
-    #th fibonacci number
+        will be too slow
+  #th fibonacci number
       f(n) = f(n-1) + f(n-2)
       0 1 1 2 3 5 8 13 21 34 35
  */
@@ -7,7 +8,7 @@
 public class GeneralWay {
     public static void main(String[] args)
     {
-        int n = 22;
+        int n = 20;
         System.out.println(fibonacci(n));
     }
 
